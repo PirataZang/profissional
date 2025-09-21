0d50b8b2456b22b838f62d1d4e5ef7af07ecb807
Igor <igorfronza20@gmail.com>
2025-09-21 11:50:08 -0300

    Ajustado para usar a api do github padrão mas localmente usar de maneira ilimitada para desenvolvimento

--- app/composables/useGithub.ts
+++ app/composables/useGithub.ts
@@ -1,35 +1,18 @@
 import collect from 'collect.js'
 
 export const useGithub = () => {
-    const runtimeConfig = useRuntimeConfig()
-    const GITHUB_TOKEN = runtimeConfig.public.GITHUB_TOKEN
+    const config = useRuntimeConfig()
 
-    // Adicione esta verificação antes de criar os headers
-    if (!GITHUB_TOKEN) {
-        console.error('GitHub token não está definido.')
-        return {
-            getUser: () => Promise.resolve(null),
-            getRepos: () => Promise.resolve(null),
-            getLanguages: () => Promise.resolve(null),
-        }
-    }
+    const headers: any = config.githubToken ? { Authorization: `token ${config.githubToken}` } : {}
 
-    const headers = {
-        Authorization: `token ${GITHUB_TOKEN}`,
-    }
+    const getUser = () => $fetch('https://api.github.com/users/PirataZang', { headers })
 
-    const getUser = async () => {
-        return await $fetch(`https://api.github.com/users/PirataZang`, { headers })
-    }
-
-    const getRepos = async () => {
-        return await $fetch(`https://api.github.com/users/PirataZang/repos`, { headers })
-    }
+    const getRepos = () => $fetch('https://api.github.com/users/PirataZang/repos', { headers })
 
     const getLanguages = async () => {
-        const repos: any = await $fetch(`https://api.github.com/users/PirataZang/repos`, { headers })
+        const repos: any = await $fetch('https://api.github.com/users/PirataZang/repos', { headers })
         return collect(repos)
-            .map((repo: any) => repo.language)
+            .map((r: any) => r.language)
             .unique()
             .all()
     }
