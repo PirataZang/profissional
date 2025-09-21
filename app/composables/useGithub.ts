9636c0d3f7df20575d83a465337c821dc5d2bf17
Igor <igorfronza20@gmail.com>
2025-09-20 22:02:04 -0300

    Ajustado env

--- app/composables/useGithub.ts
+++ app/composables/useGithub.ts
@@ -1,17 +1,27 @@
-import collect from "collect.js"
+import collect from 'collect.js'
+require('dotenv').config()
+
+const GITHUB_TOKEN = process.env.NUXT_GITHUB_TOKEN //Token de acesso da API do GitHub
 
 export const useGithub = () => {
+    const headers = {
+        Authorization: `token ${GITHUB_TOKEN}`,
+    }
+
     const getUser = async () => {
-        return await $fetch(`https://api.github.com/users/PirataZang`)
+        return await $fetch(`https://api.github.com/users/PirataZang`, { headers })
     }
 
     const getRepos = async () => {
-        return await $fetch(`https://api.github.com/users/PirataZang/repos`)
+        return await $fetch(`https://api.github.com/users/PirataZang/repos`, { headers })
     }
 
     const getLenguages = async () => {
-        const repos:any = await $fetch(`https://api.github.com/users/PirataZang/repos`);
-        return collect(repos).map((repo:any) => repo.language).unique().all()
+        const repos: any = await $fetch(`https://api.github.com/users/PirataZang/repos`, { headers })
+        return collect(repos)
+            .map((repo: any) => repo.language)
+            .unique()
+            .all()
     }
 
     return { getUser, getRepos, getLenguages }
